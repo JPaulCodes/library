a5929261d9043862e3dca95e24ac6103261e2b06
JonPaulCode <jwatson88@live.co.uk>
2022-04-04 16:42:36 +0100

    Add functionality to display added books on the page

--- scripts/library-script.js
+++ scripts/library-script.js
@@ -17,10 +17,43 @@ function addBook(title, author, pages, read) {
   myLibrary.push(book);
 }
 
+function generateBookCard(book) {
+  const bookCard = document.createElement('div');
+  const bookTitle = document.createElement('p');
+  const bookAuthor = document.createElement('p');
+  const bookPages = document.createElement('p');
+  const bookRead = document.createElement('button');
+
+  bookCard.classList.add('book-card');
+  bookTitle.innerText = book.title;
+  bookAuthor.innerText = book.author;
+  bookPages.innerText = book.pages;
+
+  if (book.read) {
+    bookRead.classList.add('finished');
+    bookRead.innerText = 'Finished';
+  } else {
+    bookRead.classList.add('not-finished');
+    bookRead.innerText = 'Not Finished';
+  }
+
+  bookCard.append(bookTitle, bookAuthor, bookPages, bookRead);
+  return bookCard;
+}
+
+function updateBookContainer() {
+  bookContainer.replaceChildren();
+  myLibrary.forEach((book) => {
+    const bookCard = generateBookCard(book);
+    bookContainer.appendChild(bookCard);
+  });
+}
+
 bookForm.addEventListener('submit', (ev) => {
   const fd = new FormData(bookForm);
 
   ev.preventDefault();
   addBook(...fd.values());
+  updateBookContainer();
   bookForm.reset();
 });
